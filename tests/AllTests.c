6c6a70987b0e7bf3c9b2d0e01492f96a6f48f334
dada0z <zhang.guangda.coder@gmail.com>
2019-06-22 00:44:34 +0800

    feature:add unit test case for mime.c

--- tests/AllTests.c
+++ tests/AllTests.c
@@ -1,12 +1,14 @@
 #include "CuTest.h"
 #include <stdio.h>
 CuSuite* hashmapGetSuite();
+CuSuite* mimeGetSuite();
 
 void RunAllTests(void) {
     CuString* output = CuStringNew();
     CuSuite* suite = CuSuiteNew();
 
     CuSuiteAddSuite(suite, hashmapGetSuite());
+    CuSuiteAddSuite(suite, mimeGetSuite());
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);
